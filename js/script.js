cc37ee86845055a20ed6939ab09d51e0d3633023
JD Tadlock <jt7903@gmail.com>
2024-03-19 12:05:21 -0400

    started conditionals

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var password = ''
+var wantsCapital = true
+var wantsLowercase = false
+var wantsNumbers = true
+var wantsSpecialCharacters = true
+
+// Storing datasets of the character values
+var lowercase = 'abcdefghijklmnopqrstuvwxyz'
+var capital = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+var numbers = '0123456789'
+var specials = '!@#$%^&*()'
+var amount = 50
+
+// 
