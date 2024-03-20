3ea7da9ae21f63e8c1671c8024913b529d892b34
JD Tadlock <jt7903@gmail.com>
2024-03-20 12:56:53 -0400

    finished if else switch overview

--- js/script.js
+++ js/script.js
@@ -1,75 +1,51 @@
-var wantsCapital = true
-var wantsLowercase = false
-var wantsNumbers = true
-var wantsSpecialCharacters = true
-var result = 1000 < 100
+// const num = 10
 
-// Storing datasets of the character values
-var lowercase = 'abcdefghijklmnopqrstuvwxyz'
-var capital = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-var numbers = '0123456789'
-var specials = '!@#$%^&*()'
-var amount = 10
-var characterSet = ''
-var password = ''
+// num++
 
-console.log('Building:', characterSet)
 
-// If conditional statements require a truthy value to trigger the code block
-// Combine all the characters they want into one string
-if (wantsCapital) {
-  characterSet += capital
-  console.log('Building:', characterSet)
-}
+// let data = 'one'
 
-if (wantsLowercase) {
-  characterSet += lowercase
-  console.log('Building:', characterSet)
-}
+// if (data === 'resolved') {
+//   console.log('data was received!')
+// } else if (data === 'one') {
+//   console.log('one!')
+// } else {
+//   console.log('data was not received...')
+// }
 
-if (wantsNumbers) {
-  characterSet += numbers
-  console.log('Building:', characterSet)
-}
+// let age = 8
 
-if (wantsSpecialCharacters) {
-  characterSet += specials
-  console.log('Building:', characterSet)
-}
-
-console.log('character set completed: ', characterSet)
-console.log('character set length: ', characterSet.length)
-// assignment, condition, incrementor/decrementor/multiplier
-
-// for (var index = 0; index < characterSet.length; index++) {
-//   console.log(characterSet[index])
+// if (age >= 18) {
+//   console.log('You are now an adult')
+// } else if ('something' && age < 18) {
+//   console.log('You are a teenager')
+// } else {
+//   console.log('You are a youngster')
 // }
 
 
-for (var count = 0; count < amount; count++) {
-  var ranNum = Math.random()
-  var float = ranNum * characterSet.length
-  var index = Math.floor(float)
-  var randomCharacter = characterSet[index]
+// let val = 'some'
+// let num = 0
 
-  console.log('random character:', randomCharacter)
+// let result = val && num
+// let anotherResult = val || num
+// let comparison = num > 0
 
-  password += randomCharacter
-}
+// console.log(comparison)
 
-console.log('Completed Password:', password)
-// var str = 'some'
-// console.log(str.length)
-// console.log(characterSet[characterSet.length - 1])
+// const num = 3
 
-// The end result will be a string that is a combination of all the character strings that they requested
-// Then pull a random character n amount of times from that combined string
-
-
-
-
-
-
+// switch (num) {
+//   case 10:
+//     console.log('yep, it is 10')
+//     console.log('something else for 10')
+//     break
+//   case 5:
+//     console.log('it is 5')
+//     break
+//   default:
+//     console.log('it is not one of the expected values')
+// }
 
 
 
@@ -78,7 +54,10 @@ console.log('Completed Password:', password)
 
 
 
+// for (var index = 0; index < 10; index++) {
+//   console.log(index)
 
-
-
-
+//   if (true) {
+//     var someVar = 'something'
+//   }
+// }
