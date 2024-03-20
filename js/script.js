0bf1f4d33d6948e8fce26fc713bc40a211e263ad
JD Tadlock <jt7903@gmail.com>
2024-03-20 10:13:53 -0400

    working on generator

--- js/script.js
+++ js/script.js
@@ -13,28 +13,32 @@ var amount = 10
 var characterSet = ''
 var password = ''
 
-// console.log(specials[3])
+console.log('Building:', characterSet)
 
 // If conditional statements require a truthy value to trigger the code block
 // Combine all the characters they want into one string
 if (wantsCapital) {
   characterSet += capital
+  console.log('Building:', characterSet)
 }
 
 if (wantsLowercase) {
   characterSet += lowercase
+  console.log('Building:', characterSet)
 }
 
 if (wantsNumbers) {
   characterSet += numbers
+  console.log('Building:', characterSet)
 }
 
 if (wantsSpecialCharacters) {
   characterSet += specials
+  console.log('Building:', characterSet)
 }
 
-// console.log(characterSet)
-console.log(characterSet.length)
+console.log('character set completed: ', characterSet)
+console.log('character set length: ', characterSet.length)
 // assignment, condition, incrementor/decrementor/multiplier
 
 // for (var index = 0; index < characterSet.length; index++) {
@@ -46,11 +50,14 @@ for (var count = 0; count < amount; count++) {
   var ranNum = Math.random()
   var float = ranNum * characterSet.length
   var index = Math.floor(float)
+  var randomCharacter = characterSet[index]
 
-  password += characterSet[index]
+  console.log('random character:', randomCharacter)
+
+  password += randomCharacter
 }
 
-console.log(password)
+console.log('Completed Password:', password)
 // var str = 'some'
 // console.log(str.length)
 // console.log(characterSet[characterSet.length - 1])
