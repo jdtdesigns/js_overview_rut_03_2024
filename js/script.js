02f9ac089ffe5c152f4d5707839256e218bebd32
JD Tadlock <jt7903@gmail.com>
2024-03-21 10:15:35 -0400

    finished array basics

--- js/script.js
+++ js/script.js
@@ -2,35 +2,41 @@
 const fruits = ['orange', 'apple', 'grape']
 const data = [10, 'one', ['John', 'Smith'], 57, null]
 
-console.log(fruits)
+// Use the assignment operator to replace an item/element in an array
+fruits[1] = 'kiwi'
 
-// Adding Items to an Array
-fruits.push('kiwi')
-
-console.log(fruits)
-
-fruits.unshift('banana')
+// Use splice to insert an item(s) at a certain index or to remove an item(s) at a certain index
+fruits.splice(1, 0, 'banana', 'strawberry')
 
 console.log(fruits)
 
-fruits.splice(1, 0, 'strawberry', 'blueberry')
+// // Adding Items to an Array
+// fruits.push('kiwi')
 
-console.log(fruits)
+// console.log(fruits)
 
-// Removing Items from an Array
-fruits.pop()
+// fruits.unshift('banana')
 
-console.log(fruits)
+// console.log(fruits)
 
-fruits.shift()
+// fruits.splice(1, 0, 'strawberry', 'blueberry')
 
-console.log(fruits)
+// console.log(fruits)
 
-fruits.splice(2, 1)
+// // Removing Items from an Array
+// fruits.pop()
 
-console.log(fruits)
+// console.log(fruits)
 
-// Iterating or Looping over an Array
-for (let i = 0; i < fruits.length; i++) {
-  console.log(fruits[i])
-}
+// fruits.shift()
+
+// console.log(fruits)
+
+// fruits.splice(2, 1)
+
+// console.log(fruits)
+
+// // Iterating or Looping over an Array
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(fruits[i])
+// }
