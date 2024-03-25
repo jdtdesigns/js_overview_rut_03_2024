02ce01c38fbf320637c2f8a3f361201300d643d1
JD Tadlock <jt7903@gmail.com>
2024-03-25 09:23:38 -0400

    03 prep complete

--- js/script.js
+++ js/script.js
@@ -1,65 +1,59 @@
 const contentWrap = document.querySelector('.content-wrap')
 const btn = document.querySelector('button.start-btn')
-const showMenu = false
-const users = [
-  {
-    firstName: 'JD',
-    lastName: 'Tadlock'
-  },
-  {
-    firstName: 'Bob',
-    lastName: 'Smith'
-  },
-  {
-    firstName: 'Jane',
-    lastName: 'Doe'
-  }
-]
 
-function randomNum() {
-  return Math.random()
+function collectPets() {
+  const animals = []
+  let keepGoing = true
+
+  while (keepGoing) {
+    let animalName
+
+    while (!animalName) {
+      animalName = prompt('Type your animal name')
+    }
+
+    const animalType = prompt('What is the animal\'s type')
+
+    const animalObj = {
+      name: animalName,
+      type: animalType
+    }
+
+    animals.push(animalObj)
+
+    const wantsToContinue = confirm('Click Ok to add another animal')
+
+    keepGoing = wantsToContinue
+  }
+
+  return animals
 }
 
-function doSomething() {
-  for (let user of users) {
-    // const div = document.createElement('div')
-    // const firstH3 = document.createElement('h3')
-    // const lastH3 = document.createElement('h3')
-
-    // firstH3.innerText = 'First Name: ' + user.firstName
-    // lastH3.innerText = 'Last Name: ' + user.lastName
-
-    // lastH3.style.backgroundColor = 'orange'
-
-    // div.append(firstH3, lastH3)
-
-    // contentWrap.append(div)
-
+function displayAllAnimals(animals) {
+  for (let animal of animals) {
+    // Insert a div for every animal object in animals and insert into the content-wrap div
     contentWrap.insertAdjacentHTML('beforeend', `
       <div>
-        <h2>First Name: ${user.firstName}</h2>
-        <h2 class="red">Last Name: ${user.lastName}</h2>
+        <h3>Name: ${animal.name}</h3>
+        <p>Type: ${animal.type}</p>
       </div>
     `)
   }
-
-
-
-
-
-
-  // const fullName = 'JD Tadlock'
-
-  // contentWrap.innerHTML = `
-  //   <div class="inner-container">
-  //     <p>inserted paragraph</p>
-  //     <p>another paragraph</p>
-  //     <p>Name: ${fullName}</p>
-  //     <p>Number: ${randomNum()}</p>
-  //     <p>${showMenu && 'menu'}</p>
-  //   </div>
-  // `
-  // console.log(contentWrap.innerHTML)
 }
 
-btn.addEventListener('click', doSomething)
+function consoleRandomAnimal(animals) {
+  // Use Math.floor and Math.random to get a random object from the animals array and display that animal data in the console
+}
+
+function startProcess() {
+  // Gathers all the animals
+  const animals = collectPets()
+
+  // Display all animals
+  displayAllAnimals(animals)
+
+  // Console out a random animal
+  consoleRandomAnimal(animals)
+}
+
+btn.addEventListener('click', startProcess)
