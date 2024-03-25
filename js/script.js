707d705046681d7a04376f230e3536975c5f3e3f
JD Tadlock <jt7903@gmail.com>
2024-03-25 12:03:58 -0400

    finished callback activity

--- js/script.js
+++ js/script.js
@@ -1,46 +1,18 @@
-// const document = {
-//   addEventListener: function (eventType, callback) {
-//     // When eventType occurs call callback
 
-//     // Event object is an object of information and tools related to the user event/action that just occured
-//     const eventObject = {
-//       mouseX: 423.23424
-//     }
-
-//     callback(eventObject)
-//   }
-// }
-
-
-
-// document.addEventListener('keypress', function (eventObj) {
-//   console.log(eventObj.mouseX)
-// })
-
-// const data = {
-//   name: 'JD',
-//   age: 44,
-//   printBirthday: function () {
-//     console.log(this.age)
-//   }
-// }
-
-// data.printBirthday()
-someFunc()
-
-function someFunc() {
-  console.log('works')
+// 2. Reference the callback function we passed
+function beginProcess(cb) {
+  const passMe = 'pass me to the callback'
+  // 3. Call the callback function and pass it the passMe variable so that the callback prints 'pass me to the callback' to the console
+  cb(passMe)
 }
 
-someFunc()
+// 1. Call beginProcess and pass it a callback function as an argument
+beginProcess(function (str) {
+  console.log(str)
+})
 
+function test(num) {
 
+}
 
-
-// function someFunc(num, callback) {
-//   callback('passed to the callback', 30, ['apple', 'orange'])
-// }
-
-// someFunc(5, function (text, num, arr) {
-//   console.log(arr[0])
-// })
+test()
