fdf98538e9162ab3b509e4b3e181ccd392871c46
JD Tadlock <jt7903@gmail.com>
2024-03-29 15:15:47 -0400

    finished drag example

--- js/script.js
+++ js/script.js
@@ -1,85 +1,189 @@
-// Select the form
-const todoForm = $('#todo-form')
-const todoOutput = $('.todo-output')
+const contentSection = $('.main-content-section')
 
-function getTodos() {
-  return JSON.parse(localStorage.getItem('todos')) || []
+function getArticles() {
+  return JSON.parse(localStorage.getItem('work')) || []
 }
 
-function outputTodos() {
-  const todos = getTodos()
 
-  // If todos are not empty, hide the empty placeholder paragraph
-  if (todos.length) {
-    $('.empty-status').hide()
+$('.drop-section, .main-content-section').droppable({
+  accept: 'article',
+  drop: function (dropEvent, ui) {
+    const articles = getArticles()
+
+    const section = $(dropEvent.target)
+    const article = $(ui.draggable[0])
+    const index = article.data('index')
+
+    section.append(article)
+
+
+    if (section.hasClass('main-content-section')) {
+      articles[index].section = 'main'
+      article.removeClass('drop')
+    } else {
+      articles[index].section = 'placeholder'
+      article.addClass('drop')
+    }
+
+    localStorage.setItem('work', JSON.stringify(articles))
   }
+})
 
-  // Empty the todos out of the output div
-  todoOutput.empty()
+function outputArticles() {
+  const articles = JSON.parse(localStorage.getItem('work'))
 
-  todos.forEach(function (todo, index) {
-    // Output a div with the text of the todo and a button to delete into the todo-output div
-    todoOutput.append(`
-      <div class="todo">
-        <h3>${todo}</h3>
-        <button data-index="${index}">Delete</button>
-      </div>
+  articles.forEach(function (article, index) {
+    const section = article.section === 'main' ? $('.main-content-section') : $('.drop-section')
+
+    section.append(`
+      <article data-index="${index}" class="col-lg-4 col-xl-3 col-md-6 ${article.section !== 'main' && 'drop'}">
+        <div class="work-item p-4 text-light" style="background-image: url('https://picsum.photos/id/24${index}/800/400');">
+          <h3>${article.title}</h3>
+          <p>${article.description}</p>
+        </div>
+      </article>
     `)
   })
 
+  $('section article').draggable({
+    // opacity: 0.7,
+    zIndex: 100,
+    helper: function (eventObj) {
+      const original = $(eventObj.target).hasClass('ui-draggable')
+        ? $(eventObj.target)
+        : $(eventObj.target).closest('.ui-draggable');
+
+      return original.clone().css({
+        maxWidth: original.outerWidth(),
+      });
+    }
+  })
 }
 
-// Function that stores todo
-function addTodo(eventObj) {
-  eventObj.preventDefault()
-  // Grab the todo text from the input
-  const todoInput = $('#todo-input')
-  const text = todoInput.val()
+outputArticles()
 
-  // Retrieve old todos
-  const todos = getTodos()
 
-  // Add the the todo to the todos array
-  todos.push(text)
 
-  // Overwrite/Save the todos array to localStorage
-  localStorage.setItem('todos', JSON.stringify(todos))
 
-  // Empty the input to get ready for the next todo
-  todoInput.val('')
 
-  // Output the todos to the DOM
-  outputTodos()
-}
 
-function deleteTodo() {
-  const btn = $(this) // eventObj.target
-  // Get the old todos
-  const todos = getTodos()
-  // Get the index of the todo that needs to be deleted
-  const index = btn.data('index')
 
-  // Splice our todos array, using the index
-  todos.splice(index, 1)
 
-  // Overwrite/Save the todos array to localStorage
-  localStorage.setItem('todos', JSON.stringify(todos))
 
-  // Remove the todo div of the todo that was deleted
-  // - Grab the parent of the button and remove it
-  btn.parent().remove()
 
-  // If no more todos, show the empty status paragraph
-  if (!todos.length) {
-    $('.empty-status').show()
-  }
-}
 
-// Add a submit listener to form that triggers a function that stores/saves the todo
-todoForm.on('submit', addTodo)
-// Add a click listener on the todo output div that gets triggered if a delete button is pressed within the div
-// Event Delegation
-todoOutput.on('click', 'button', deleteTodo)
 
-// Output todos
-outputTodos()
+
+// const workItems = [
+//   {
+//     title: 'Cool App',
+//     description: 'Description work 1',
+//     section: 'placeholder'
+//   },
+//   {
+//     title: 'Cool App2',
+//     description: 'Description work 2',
+//     section: 'main'
+//   },
+//   {
+//     title: 'Cool App3',
+//     description: 'Description work 3',
+//     section: 'main'
+//   }
+// ]
+
+// localStorage.setItem('work', JSON.stringify(workItems))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const quoteOutput = $('#quote-output')
+// const quoteBtn = $('#quote-btn')
+
+// function outputQuote(data) {
+//   quoteOutput.text(data.content)
+//   console.log('output')
+// }
+
+// async function getQuote() {
+//   const url = 'https://api.quotable.io/random'
+//   // const url = 'https://swapi.dev/api/people'
+//   const res = await fetch(url) // returns a promise object
+//   const data = await res.json()
+
+//   return data
+// }
+
+// getQuote()
+//   .then(outputQuote)
+
+
+// quoteBtn.on('click', function () {
+//   getQuote().then(outputQuote)
+// })
+
+
+
+
+// console.log('done')
+// const promise = {
+//   then: function(callback) {
+//     // Once the value comes back or whatever thing is done
+//     callback(resolvedOrFinishedValue)
+//   },
+
+//   catch: function() {
+
+//   }
+// }
+
+// promise.then(function(value) {
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const quotes = [
+//   'Bootstrap rocks',
+//   'Web development rules',
+//   'Coding is rad'
+// ]
+
+// var quote = quotes[Math.floor(Math.random() * quotes.length)]
