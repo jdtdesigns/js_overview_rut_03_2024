4067efd65420961e5ffe9cd6cc592bc1dfe5cb26
JD Tadlock <jt7903@gmail.com>
2024-04-01 11:42:01 -0400

    finished drag-drop overview

--- js/script.js
+++ js/script.js
@@ -1,110 +1,212 @@
-// Target the two divs with jQuery
+const todoForm = $('form')
+const todoSection = $('.to-do')
+const doneSection = $('.done')
 
-// Create a function that gets todos from localStorage
+function getTodos() {
+  const raw = localStorage.getItem('todos')
+  const parsed = JSON.parse(raw) || []
 
-// Create a function that outputs a paragraph for each todo in localStorage into main when the page loads
-
-/**
-* Use the draggable listener on the main div paragraphs when they are dragged 
-- Your draggable helper callback is going to return a clone of the paragraph
-*/
-
-
-/**
-* Listen with droppable on both main and other when a paragraph is dropped
-
-- Grab the parent div that you are dropping the paragraph into
-  - Checking the dropObject.target
-- Grab that's being dragged (ui.draggable[0])
-- Append the paragraph to the div that you are dropping into
-- 
-*/
-
-// STEP 2 FOR LOCALSTORAGE
-/**
-* Store to localStorage an array of todo objects
-- Each object is going to have 2 properties (text, status)
-  - status property either equal to 'main' or 'other'
-
-* When the page loads, call your output as above and output a paragraph for each object in the todos object array
-  - Set a data-index attribute on each paragraph of the index value of that object in the array
-  - If the status of the todo object is 'main', then output the paragraph into the main div, otherwise into other div
-
-* When a paragraph is dropped, you do the standard steps above to drop it into the new div, but also update the localStorage object status property with the new value, based on the div you drop it in (getting the div parent class)
-  - Grab all the todos
-  - Update the todo object in the todos array based on the data-index that was stored to the paragraph that you're dropping
-  - todos[paragraphDataIndex].status = (conditional statement determining setting either 'main' or 'other')
-  - Overwrite the old array with the new
-*/
-
-
-
-
-
-
-const contentSection = $('.main-content-section')
-
-function getArticles() {
-  return JSON.parse(localStorage.getItem('work')) || []
+  return parsed
 }
 
 
-$('.drop-section, .main-content-section').droppable({
-  accept: 'article',
-  drop: function (dropEvent, ui) {
-    const articles = getArticles()
-
-    const section = $(dropEvent.target)
-    const article = $(ui.draggable[0])
-    const index = article.data('index')
-
-    section.append(article)
-
-
-    if (section.hasClass('main-content-section')) {
-      articles[index].section = 'main'
-      article.removeClass('drop')
-    } else {
-      articles[index].section = 'placeholder'
-      article.addClass('drop')
-    }
-
-    localStorage.setItem('work', JSON.stringify(articles))
-  }
-})
-
-function outputArticles() {
-  const articles = JSON.parse(localStorage.getItem('work'))
-
-  articles.forEach(function (article, index) {
-    const section = article.section === 'main' ? $('.main-content-section') : $('.drop-section')
-
-    section.append(`
-      <article data-index="${index}" class="col-lg-4 col-xl-3 col-md-6 ${article.section !== 'main' && 'drop'}">
-        <div class="work-item p-4 text-light" style="background-image: url('https://picsum.photos/id/24${index}/800/400');">
-          <h3>${article.title}</h3>
-          <p>${article.description}</p>
-        </div>
-      </article>
-    `)
-  })
-
-  $('section article').draggable({
-    // opacity: 0.7,
+function setupDrag(els) {
+  els.draggable({
+    opacity: 0.7,
     zIndex: 100,
-    helper: function (eventObj) {
-      const original = $(eventObj.target).hasClass('ui-draggable')
-        ? $(eventObj.target)
-        : $(eventObj.target).closest('.ui-draggable');
+    revert: true
+  })
+}
 
-      return original.clone().css({
-        maxWidth: original.outerWidth(),
-      });
+function setupDrop() {
+  $('.to-do, .done').droppable({
+    accept: '.item',
+    drop: function (eventObj, ui) {
+      const section = $(eventObj.target)
+      const paragraph = $(ui.draggable[0])
+      const index = paragraph.data('index')
+
+      paragraph.css({
+        position: 'relative',
+        top: 0,
+        left: 0
+      })
+
+      section.append(paragraph)
+
+      const todos = getTodos()
+
+      const todoObj = todos[index]
+
+      if (section.hasClass('to-do')) {
+        todoObj.status = 'to-do'
+      } else {
+        todoObj.status = 'done'
+      }
+
+      localStorage.setItem('todos', JSON.stringify(todos))
     }
   })
 }
 
-outputArticles()
+function addTodo(eventObj) {
+  eventObj.preventDefault()
+
+  const todoInput = $('#todo-input')
+  const todoText = todoInput.val()
+  const paragraph = $(`<p class="item">${todoText}</p>`)
+
+  setupDrag(paragraph)
+
+  todoSection.append(paragraph)
+  todoInput.val('')
+
+  const todos = getTodos() // []
+
+  const newTodoObject = {
+    text: todoText,
+    status: 'to-do'
+  }
+
+  todos.push(newTodoObject)
+
+  localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+function outputTodos() {
+  const todos = getTodos()
+
+  todos.forEach(function (todoObj, index) {
+    const paragraph = $(`<p data-index="${index}" class="item">${todoObj.text}</p>`)
+    // Target the section based on the status
+    //  and append them to that section
+    if (todoObj.status === 'to-do') {
+      todoSection.append(paragraph)
+    } else {
+      doneSection.append(paragraph)
+    }
+
+  })
+}
+
+function initialProcesses() {
+  setupDrop()
+  outputTodos()
+  setupDrag($('section p'))
+
+  todoForm.on('submit', addTodo)
+}
+
+initialProcesses()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// helper: function (eventObj) {
+//   const paragraphBeingDragged = eventObj.target
+//   const jQueryVersion = $(paragraphBeingDragged)
+
+//   const copy = jQueryVersion.clone().css({
+//     maxWidth: jQueryVersion.outerWidth(),
+//   })
+
+//   return copy
+// }
+// document.querySelectorAll('p').addEventListener('drag', function() {
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const contentSection = $('.main-content-section')
+
+// function getArticles() {
+//   return JSON.parse(localStorage.getItem('work')) || []
+// }
+
+
+// $('.drop-section, .main-content-section').droppable({
+//   accept: 'article',
+//   drop: function (dropEvent, ui) {
+//     const articles = getArticles()
+
+//     const section = $(dropEvent.target)
+//     const article = $(ui.draggable[0])
+//     const index = article.data('index')
+
+//     section.append(article)
+
+
+//     if (section.hasClass('main-content-section')) {
+//       articles[index].section = 'main'
+//       article.removeClass('drop')
+//     } else {
+//       articles[index].section = 'placeholder'
+//       article.addClass('drop')
+//     }
+
+//     localStorage.setItem('work', JSON.stringify(articles))
+//   }
+// })
+
+// function outputArticles() {
+//   const articles = JSON.parse(localStorage.getItem('work'))
+
+//   articles.forEach(function (article, index) {
+//     const section = article.section === 'main' ? $('.main-content-section') : $('.drop-section')
+
+//     section.append(`
+//       <article data-index="${index}" class="col-lg-4 col-xl-3 col-md-6 ${article.section !== 'main' && 'drop'}">
+//         <div class="work-item p-4 text-light" style="background-image: url('https://picsum.photos/id/24${index}/800/400');">
+//           <h3>${article.title}</h3>
+//           <p>${article.description}</p>
+//         </div>
+//       </article>
+//     `)
+//   })
+
+//   $('section article').draggable({
+//     // opacity: 0.7,
+//     zIndex: 100,
+//     helper: function (eventObj) {
+//       const original = $(eventObj.target).hasClass('ui-draggable')
+//         ? $(eventObj.target)
+//         : $(eventObj.target).closest('.ui-draggable');
+
+//       return original.clone().css({
+//         maxWidth: original.outerWidth(),
+//       });
+//     }
+//   })
+// }
+
+// outputArticles()
 
 
 
